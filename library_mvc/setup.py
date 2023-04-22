912ff712e1544bdd48a3cb681f1b32e43b78ef5b
simon1494 <105741786+simon1494@users.noreply.github.com>
2023-04-21 21:48:31 -0300

    New setup
    Added a setup.py file

---
+++ library_mvc/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="Hermes BS",
+    version="1.0.0",
+    py_modules=["controler", "model", "template"],
+    author="Simon Bierozko",
+    author_email="simon1494@hotmail.com",
+    url="https://github.com/simon1494",
+    description="Sample app for organizing a home library",
+)
